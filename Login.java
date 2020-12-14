ddad08443774fc96be50348611276c1467a8beca
Nimarta <nimarta.bcss19@iba-suk.edu.pk>
2020-12-14 18:46:03 +0500

    feature/front-end add Login.java file

---
+++ Login.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import java.awt.event.*;
+import static javax.swing.JOptionPane.showMessageDialog;
+
+class Login implements ActionListener
+{
+	JFrame f;
+	Container c;
+	JLabel PLabel, menuTitle, 	
+}
